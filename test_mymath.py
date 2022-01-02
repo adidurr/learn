9748a6df7aeebccde424897f788c9943955c0c7d
jtfighting <adidurr@gmail.com>
2022-01-02 13:50:04 -0800

    Updated for pytest

--- test_mymath.py
+++ test_mymath.py
@@ -1,12 +1,4 @@
-from unittest import TestCase, main
 from mymath import add
 
-class SimpleTest(TestCase):
-        def test_add(self):
-                self.assertEqual(add(4,5), 9)
-                self.assertEqual(add(0,0), 0)
-                self.assertEqual(add(0,-1), -1)
-                self.assertEqual(add(0,-1.1), -1.1)
-
-if __name__ == '__main__':
-        main()
+def test_add():
+    assert add(4,5) ==  9
