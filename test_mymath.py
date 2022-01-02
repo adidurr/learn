5aec72c4fd6ee3fcccaa587a09d0c3ec80d5371a
jtfighting <adidurr@gmail.com>
2022-01-02 11:27:00 -0800

    updated tests

--- test_mymath.py
+++ test_mymath.py
@@ -4,6 +4,9 @@ from mymath import add
 class SimpleTest(TestCase):
         def test_add(self):
                 self.assertEqual(add(4,5), 9)
+                self.assertEqual(add(0,0), 0)
+                self.assertEqual(add(0,-1), -1)
+                self.assertEqual(add(0,-1.1), -1.1)
 
 if __name__ == '__main__':
         main()
